bedb861c4f32e3a5eb96b9248f66985d22eb796c
iHxtch <84823076+iHxtch@users.noreply.github.com>
2024-01-21 22:37:39 -0500

    api update and boost fix

--- src/main/java/org/insurgencedev/ecoquestsboost/EcoQuestsBoostAddon.java
+++ src/main/java/org/insurgencedev/ecoquestsboost/EcoQuestsBoostAddon.java
@@ -1,21 +1,17 @@
 package org.insurgencedev.ecoquestsboost;
 
-import org.bukkit.Bukkit;
+import org.insurgencedev.ecoquestsboost.listeners.QuestsExperienceGainListener;
 import org.insurgencedev.insurgencesets.api.addon.ISetsAddon;
 import org.insurgencedev.insurgencesets.api.addon.InsurgenceSetsAddon;
 import org.insurgencedev.insurgencesets.libs.fo.Common;
 
-@ISetsAddon(name = "EcoQuestsBoost", version = "1.0.0", author = "Insurgence Dev Team", description = "Boost the quest experience earned from EcoQuests")
+@ISetsAddon(name = "EcoQuestsBoost", version = "1.0.1", author = "Insurgence Dev Team", description = "Boost the quest experience earned from EcoQuests")
 public class EcoQuestsBoostAddon extends InsurgenceSetsAddon {
 
     @Override
     public void onAddonStart() {
-        if (isDependentEnabled()) {
+        if (Common.doesPluginExist("EcoQuests")) {
             registerEvent(new QuestsExperienceGainListener());
         }
-    }
-
-    private boolean isDependentEnabled() {
-        return Bukkit.getPluginManager().isPluginEnabled("EcoQuests");
     }
 }
